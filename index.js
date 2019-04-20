511ca7ccd5ae4a7b85e25a5b16d45d11d7fd4bc1
wideo21 <iii2908@naver.com>
2019-04-20 19:44:52 +0900

    퍼브리싱

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
+//main.ejs 를 퍼블리싱해서 채팅사이트 처럼 만들것!
+//엔터키시 전송될 수 있도록 할것.
+//개인별로 채팅글자 색상 
+//채팅냉여 스크롤
 let express = require('express');
 let http = require('http');
 let path = require('path');
 
 let app = express();
+let server = http.Server(app);
+
+let io = require('socket.io')(server);
+
+
 app.set('view engine', 'ejs');
+
 app.set('views', path.join(__dirname, 'view'));
 
 app.get('/main', (req, res) => {
     res.render('main', {msg:'Welcome to express 4!'});
 });
 
-http.createServer(app).listen(10500, ()=>{
-    console.log("서버 실행중 : 10500포트");
+
+let nickList = ['수달', '멍멍', '곤달', '스트', '유튜버'];
+//소켓 작업
+io.on('connection', (socket) =>{
+    console.log(socket.id);
+    console.log("클라이언트 접속");
+    let idx = Math.floor(Math.random() * nickList.length);
+    let randomNick = nickList[idx];
+    socket.nickname = randomNick;
+
+    socket.on('change', (data)=>{
+        let name = data.name;
+        socket.nickname = name ;
+        
+    });
+
+    socket.on('gondr', (data)=>{
+        let msg = `${socket.nickname} : ${data.text}`;
+        io.emit('server-msg', {text: msg});
+    })
+
+    socket.on('disconnect', ()=>{
+        console.log("유저 연결 종료");
+    });
+});
+
+server.listen(10500, ()=>{
+    console.log("서버가 10500포트에서 실행중입니다.");
 });
